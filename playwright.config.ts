4e2010d94176dcfb0721aabadd39eded5c2c71bb
KanishkaWickramasinghe <kanishkapicaros@gmail.com>
2025-03-19 15:35:03 +0800

    config file chnages

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL:urls.careerCoachUrl,
@@ -31,6 +31,7 @@ export default defineConfig({
     actionTimeout: 500000,
     navigationTimeout: 3000000,
     screenshot:'only-on-failure',
+    video:'retain-on-failure',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
